195e0a976467059ba2a614f13cbba54f8c8c9c86
Hackbright Student <info@hackbrightacademy.com>
2016-05-06 13:32:50 -0700

    Completed unit test and added comments

---
+++ model.py
@@ -0,0 +1,44 @@
+from flask.ext.sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class Game(db.Model):
+    """Board game."""
+
+    __tablename__ = "games"
+    game_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(20), nullable=False, unique=True)
+    description = db.Column(db.String(100))
+
+
+def connect_to_db(app, db_uri="postgresql:///games"):
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+    db.app = app
+    db.init_app(app)
+
+
+def example_data():
+    """Create example data for the test database."""
+    #FIXME: write a function that creates a game and adds it to the database.
+
+    #Don't do the below. Since this function belongs to the class Game, it inherits
+    #its attributes
+    # __tablename__ = "games"
+    # game_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    # name = db.Column(db.String(20), nullable=False, unique=True)
+    # description = db.Column(db.String(100))
+
+    monopoly = Game(name='monopoly', description='real estate game')
+
+    #DO NOT USE THE METHOD .add_all([...]) or your tests will FAIL!
+    db.session.add(monopoly)
+    db.session.commit()
+
+
+
+if __name__ == '__main__':
+    from party import app
+
+    connect_to_db(app)
+    print "Connected to DB."
